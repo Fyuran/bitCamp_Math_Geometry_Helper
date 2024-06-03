288f2b018470be04f1015bb9d106e67e12806014
MauroGariazzo <mauromggariazzo@gmail.com>
2024-06-03 19:43:19 +0200

    Update calculator & exercises
    aggiunto files css, html, js per calcolatrice, ed esercizi.

---
+++ js/calculatorScript.js
@@ -0,0 +1,102 @@
+let currentNumber = "";
+let previousNumber = "";
+let result = "";
+let lastOperator = "";
+
+
+function appendNumber(number) {
+    if (lastOperator == "" && result != "") {
+        clearDisplay();
+    }
+
+    if (number == "." && !(currentNumber.includes("."))) {
+        if (currentNumber == "") {
+            currentNumber = "0" + number; //esempio 0.5
+        }
+
+        else {
+            currentNumber += number; //->esempio 3.5
+        }
+    }
+
+    else if (number == "." && currentNumber.includes(".")) {
+        return; //c'� gi� il punto
+    }
+
+    else {
+        currentNumber += number; // altri caratteri che non sono un "."
+    }
+
+    updateDisplay();
+
+}
+
+function setOperation(operator) {
+    if (previousNumber == "") //se previous number � uguale a "" , allora non ho ancora il secondo operando per eseguire l'operazione
+    {
+        previousNumber = currentNumber; //passo il numero inserito dall'utente che � contenuto in currentNumber a previousNumber
+
+    }
+
+    else {
+        //sia previousNumber che currentNumber sono valorizzati, quindi posso eseguire l'operazione
+
+        calculate();
+
+        //passo il risultato a previousNumber, in modo che quando dovr� concatenare ulteriori operazioni, il primo operando sia sempre riportato su previousNumber e il secondo(ultimo) sia su currentNumber
+        previousNumber = result;
+
+        //resetto currentNumber per un nuovo inserimento
+        currentNumber = "";
+    }
+
+    lastOperator = operator; //salvo l'operatore per sapere dentro il metodo calculate che tipo di operazione devo eseguire
+    currentNumber = ""; //resetto currentNumber per poterlo sfruttare nell'inserimento di un nuovo numero
+}
+
+function calculate() {
+    if (currentNumber != "" && previousNumber != "") {
+        switch (lastOperator) {
+            case "+":
+                result = parseFloat(previousNumber) + parseFloat(currentNumber);
+                break;
+
+            case "-":
+                result = parseFloat(previousNumber) - parseFloat(currentNumber);
+                break;
+
+            case "*":
+                result = parseFloat(previousNumber) * parseFloat(currentNumber);
+                break;
+
+            case "/":
+                result = parseFloat(previousNumber) / parseFloat(currentNumber);
+                break;
+        }
+
+        updateResult();
+        lastOperator = "";
+    }
+}
+
+function clearDisplay() {
+
+    display.innerText = "";
+    clearData();
+}
+
+function clearData() {
+    currentNumber = "";
+    previousNumber = "";
+    result = "";
+    lastOperator = "";
+}
+
+
+function updateDisplay() {
+    display.innerText = currentNumber;
+}
+
+function updateResult() {
+    display.innerText = result;
+}
---
+++ js/exercisesScript.js
@@ -0,0 +1,98 @@
+var guessedAnswersCount;
+var wrongAnswersCount;
+
+var correctAnswers = 
+{
+    question1: 4,
+    question2: 1,
+    question3: 8,
+    question4: 3,
+    question5: 40,
+    question6: 40,
+    question7: 25.12
+};
+
+function checkAnswers() {            
+    guessedAnswersCount = 0;
+    wrongAnswersCount = 0;
+
+    //input utente
+    let radios1 = document.getElementsByName('question1'); //radiobutton prima domanda
+    let radios2 = document.getElementsByName('question2'); //radiobutton seconda domanda
+    let radios3 = document.getElementsByName('question3'); //radiobutton terza domanda
+    let radios4 = document.getElementsByName('question4'); //radiobutton quarta domanda
+    let radios5 = document.getElementsByName('question5'); //radiobutton quinta domanda
+    let text6 = document.getElementsByName('rectangleQuestion')[0]; //textarea1(trim per togliere gli spazi)
+    let text7 = document.getElementsByName('circleQuestion')[0]; //textarea2(trim per togliere gli spazi)
+
+    //metodo della classe string replace per sostituire la virgola con il punto per i numeri con decimali
+    let floatText6 = text6.value.trim().replace(',' , '.');
+    let floatText7 = text7.value.trim().replace(',' , '.');
+    floatText6 = parseFloat(floatText6); //trasformo il testo in un numero float
+    floatText7 = parseFloat(floatText7); //trasformo il testo in un numero float
+
+    /*faccio riferimento ai paragrafi per i risultati tramite id*/
+    let guessedAnswers = document.getElementById("guessedAnswers");
+    let wrongAnswers = document.getElementById("wrongAnswers");
+                            
+    /*richiamo la funzione per verificare il risultato dei radiobuttons*/
+    checkRadioAnswers(radios1, 'question1');
+    checkRadioAnswers(radios2, 'question2');
+    checkRadioAnswers(radios3, 'question3');
+    checkRadioAnswers(radios4, 'question4');
+    checkRadioAnswers(radios5, 'question5');
+    
+    /*richiamo la funzione per verificare il risultato dei textAreas*/
+    checkTextAnswers(floatText6, 'question6', text6);
+    checkTextAnswers(floatText7, 'question7', text7);
+
+    /*aggiorno i valori dei paragrafi*/
+    guessedAnswers.innerHTML = "giuste: " + guessedAnswersCount + "/7";
+    wrongAnswers.innerHTML = "sbagliate: " + wrongAnswersCount + "/7";
+    
+    console.log("Risposte corrette: " + guessedAnswersCount);
+    console.log("Risposte sbagliate: " + wrongAnswersCount);
+}
+
+function checkRadioAnswers(radios, question) {
+    let radioSelected = false;
+
+    /*ciclo nei radiobuttons di ogni cards*/
+    for(let i = 0; i < radios.length; i++) {
+        //se quel radiobutton è attivo (checkato)
+        if(radios[i].checked) {
+            radioSelected = true;
+            //ed è uguale al valore corretto
+            if(radios[i].value == correctAnswers[question]) {
+                guessedAnswersCount++; //aumenta il numero di risposte indovinate
+            } else {
+                wrongAnswersCount++; //aumenta il numero di risposte sbagliate
+            }
+        }
+    }
+
+    if(!radioSelected) {
+        radios[0].classList.add('is-invalid');
+        radios[1].classList.add('is-invalid');
+        radios[2].classList.add('is-invalid');
+    } else {
+        radios[0].classList.remove('is-invalid');
+        radios[1].classList.remove('is-invalid');
+        radios[2].classList.remove('is-invalid');
+    }
+}
+
+function checkTextAnswers(textValue, question, textElement) {
+    if (Number.isNaN(textValue)) {
+        textElement.classList.add('is-invalid');
+        return;
+    }
+
+    textElement.classList.remove('is-invalid');
+
+    if (textValue == correctAnswers[question]) {
+        guessedAnswersCount++;
+    } else {
+        wrongAnswersCount++;
+    }
+}
